75014e52c5a6f30836c886936f5972ed5c740588
Tyler Sheffels <Tyler.Sheffels@gmail.com>
2014-06-23 19:38:24 -0400

    Ordering date entries

--- server/databaseAccess/tracker_access.js
+++ server/databaseAccess/tracker_access.js
@@ -6,10 +6,36 @@ addTracker = function(trackerAttributes) {
   });
 };
 addDataToTracker = function(attributes) {
-  var date = attributes.date
+  var date = roundDate(attributes.date)
   var vote = attributes.vote
   var data = [date, vote]
+  
+  //extract the old history
+  prevHistory = Trackers.findOne( {_id: attributes.trackerId} ).history || []
+
+  //Remove an old entry for this date
+  newHistory = _.reject(prevHistory, function(item) {
+    return item[0] === date;
+  })
+
+  //Insert the new value
+  indexToInsertAt = _.sortedIndex(newHistory, data, function(item) {
+    return item[0]
+  })
+
+  newHistory.splice(indexToInsertAt, 0, data)
 
   return Trackers.update( {_id: attributes.trackerId},
-      {$push: {history: data } } )
+      {$set: {history: newHistory } } )
 };
+
+
+//helpers
+var roundDate = function(date) {
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
+  return date;
+}
